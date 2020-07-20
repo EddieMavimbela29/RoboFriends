87014386039d0fb7bc7e5be211004d5cb79a2d52
unknown <mkholowthandow@gmail.com>
2020-07-21 01:00:40 +0200

    Adding all files

---
+++ src/containers/App.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import SearchBox from '../components/SearchBox';
+import CardList from '../components/Cardlist';
+import Scroll from '../components/Scroll';
+import '../containers/App.css'
+
+ class App extends Component {  
+    
+   constructor(){
+     super()
+
+     this.state = {
+       robots: [], 
+       searchField: ''
+     }
+   }
+
+   componentDidMount(){
+     fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => {
+        return response.json();
+      })
+      .then(users => {
+        this.setState({robots: users})
+      })
+   }
+
+   onSearchChange = (event) => {
+     this.setState({searchField: event.target.value});
+   };
+
+   render() {
+
+    const { robots, searchField } = this.state;
+  
+    const filteredRobots = robots.filter( robot => {
+        return robot.name.toLowerCase().includes(searchField.toLowerCase());
+    });
+
+    if( !robots.length ){
+      return <h1> Loading</h1>
+    }else{
+    return (
+         <div className="tc">
+             <h1 className='f2'> RoboFriends </h1>
+             <SearchBox searchChange={this.onSearchChange}/>
+             <Scroll>
+               <CardList robots={filteredRobots} />
+             </Scroll>     
+         </div>
+    );
+    }     
+  }
+    
+}
+
+
+    
+  
+export default App;
---
+++ src/robots.js
@@ -0,0 +1,38 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Andy Mavimbela',
+        username: 'Mbuziman',
+        email: 'mbuzi@g.com'
+    },
+    {
+        id: 2,
+        name: 'Eddy Mavimbela',
+        username: 'Sixty',
+        email: 'oak@g.com'
+    },
+    {
+        id: 3,
+        name: 'Trudy Mavimbela',
+        username: 'sebenza',
+        email: 'princess@g.com'
+    },
+    {
+        id: 4,
+        name: 'Andy Mavimbela',
+        username: 'Mbuziman',
+        email: 'mbuzi@g.com'
+    },
+    {
+        id: 5,
+        name: 'Andy Mavimbela',
+        username: 'Mbuziman',
+        email: 'mbuzi@g.com'
+    },
+    {
+        id: 6,
+        name: 'Andy Mavimbela',
+        username: 'Mbuziman',
+        email: 'mbuzi@g.com'
+    },
+]
